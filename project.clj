377181c5ca7869263e7b76e4d11eb2a4c2081412
taylodl <donald_l_taylor_jr@yahoo.com>
2019-12-29 19:30:57 -0500

    update clojure version dependency so CIDER works

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject midi-sequencer "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Using MIDI in Java"
+  :url "https://taylodl.wordpress.com/2014/01/21/making-music-with-clojure-an-introduction-to-midi/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]]
   :main midi-sequencer.core)
